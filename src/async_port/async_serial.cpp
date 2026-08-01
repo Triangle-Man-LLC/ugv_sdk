961c999a92a2c0302bcc60e2b69c0617aad21853
pwees-entity <farmerpus1@gmail.com>
2026-07-31 17:07:19 -0700

    Removed ASIO, fix builds

--- src/async_port/async_serial.cpp
+++ src/async_port/async_serial.cpp
@@ -1,160 +1,201 @@
-/*
- * async_serial.cpp
- *
- * Created on: Sep 10, 2020 13:00
- * Description:
- *
- * Copyright (c) 2020 Weston Robot Pte. Ltd.
- */
-
 #include "ugv_sdk/details/async_port/async_serial.hpp"
 
-#if defined(__linux__)
-#include <linux/serial.h>
-#endif
-
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <sys/ioctl.h>
 #include <cstring>
 #include <iostream>
 
 namespace westonrobot {
 
 AsyncSerial::AsyncSerial(std::string port_name, uint32_t baud_rate)
-    : port_(port_name), baud_rate_(baud_rate), serial_port_(io_context_) {}
+    : port_(std::move(port_name)),
+      baud_rate_(baud_rate),
+      hwflow_(false),
+      serial_fd_(-1),
+      epoll_fd_(-1),
+      running_(false),
+      tx_in_progress_(false) {}
 
-AsyncSerial::~AsyncSerial() { Close(); }
-
-void AsyncSerial::SetBaudRate(unsigned baudrate) {
-  serial_port_.set_option(asio::serial_port_base::baud_rate(baudrate));
+AsyncSerial::~AsyncSerial() {
+    Close();
 }
 
-void AsyncSerial::SetHardwareFlowControl(bool enabled) { hwflow_ = enabled; }
+void AsyncSerial::SetBaudRate(unsigned baudrate) {
+    baud_rate_ = baudrate;
+}
+
+void AsyncSerial::SetHardwareFlowControl(bool enabled) {
+    hwflow_ = enabled;
+}
 
 bool AsyncSerial::Open() {
-  using SPB = asio::serial_port_base;
+    if (port_opened_) return true;
 
-  try {
-    serial_port_.open(port_);
+    // open serial port
+    serial_fd_ = ::open(port_.c_str(), O_RDWR | O_NONBLOCK | O_NOCTTY);
+    if (serial_fd_ < 0) {
+        std::cerr << "Failed to open serial port\n";
+        return false;
+    }
 
-    // Set baudrate and 8N1 mode
-    serial_port_.set_option(SPB::baud_rate(baud_rate_));
-    serial_port_.set_option(SPB::character_size(8));
-    serial_port_.set_option(SPB::parity(SPB::parity::none));
-    serial_port_.set_option(SPB::stop_bits(SPB::stop_bits::one));
-    serial_port_.set_option(SPB::flow_control(
-        (hwflow_) ? SPB::flow_control::hardware : SPB::flow_control::none));
+    // configure termios
+    struct termios tio {};
+    tcgetattr(serial_fd_, &tio);
+
+    cfmakeraw(&tio);
+
+    speed_t speed = B115200;
+    switch (baud_rate_) {
+        case 9600: speed = B9600; break;
+        case 19200: speed = B19200; break;
+        case 38400: speed = B38400; break;
+        case 57600: speed = B57600; break;
+        case 115200: speed = B115200; break;
+        case 230400: speed = B230400; break;
+        default: speed = B115200; break;
+    }
+
+    cfsetispeed(&tio, speed);
+    cfsetospeed(&tio, speed);
+
+    if (hwflow_) tio.c_cflag |= CRTSCTS;
+    else tio.c_cflag &= ~CRTSCTS;
+
+    tcsetattr(serial_fd_, TCSANOW, &tio);
 
 #if defined(__linux__)
-    // Enable low latency mode on Linux
-    {
-      int fd = serial_port_.native_handle();
-      struct serial_struct ser_info;
-      ioctl(fd, TIOCGSERIAL, &ser_info);
-      ser_info.flags |= ASYNC_LOW_LATENCY;
-      ioctl(fd, TIOCSSERIAL, &ser_info);
+    // enable low latency mode
+    struct serial_struct ser {};
+    if (ioctl(serial_fd_, TIOCGSERIAL, &ser) == 0) {
+        ser.flags |= ASYNC_LOW_LATENCY;
+        ioctl(serial_fd_, TIOCSSERIAL, &ser);
     }
 #endif
 
+    // create epoll
+    epoll_fd_ = epoll_create1(0);
+    if (epoll_fd_ < 0) {
+        std::cerr << "Failed to create epoll\n";
+        ::close(serial_fd_);
+        serial_fd_ = -1;
+        return false;
+    }
+
+    struct epoll_event ev {};
+    ev.events = EPOLLIN;
+    ev.data.fd = serial_fd_;
+    epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, serial_fd_, &ev);
+
+    running_ = true;
     port_opened_ = true;
-    std::cout << "Start listening to port: " << port_ << "@" << baud_rate_
-              << std::endl;
-  } catch (std::system_error &e) {
-    std::cout << e.what() << std::endl;
-    return false;
-  }
 
-  // give some work to io_service to start async io chain
-#if ASIO_VERSION < 101200L
-  io_context_.post(std::bind(&AsyncSerial::ReadFromPort, this));
-#else
-  asio::post(io_context_, std::bind(&AsyncSerial::ReadFromPort, this));
-#endif
+    io_thread_ = std::thread(&AsyncSerial::IoThreadFunc, this);
 
-  // start io thread
-  io_thread_ = std::thread([this]() { io_context_.run(); });
-
-  return true;
+    std::cout << "AsyncSerial listening on " << port_ << "@" << baud_rate_ << "\n";
+    return true;
 }
 
 void AsyncSerial::Close() {
-  io_context_.stop();
-  if (io_thread_.joinable()) io_thread_.join();
-  io_context_.reset();
-  
-  if (IsOpened()) {
-    serial_port_.cancel();
-    serial_port_.close();
-  }
+    running_ = false;
 
-  port_opened_ = false;
+    if (io_thread_.joinable())
+        io_thread_.join();
+
+    if (epoll_fd_ >= 0) {
+        ::close(epoll_fd_);
+        epoll_fd_ = -1;
+    }
+
+    if (serial_fd_ >= 0) {
+        ::close(serial_fd_);
+        serial_fd_ = -1;
+    }
+
+    port_opened_ = false;
 }
 
-bool AsyncSerial::IsOpened() const { return serial_port_.is_open(); }
+bool AsyncSerial::IsOpened() const {
+    return port_opened_;
+}
 
-void AsyncSerial::DefaultReceiveCallback(uint8_t *data, const size_t bufsize,
-                                         size_t len) {}
+void AsyncSerial::DefaultReceiveCallback(uint8_t *data, const size_t bufsize, size_t len) {
+    // default: do nothing
+}
+
+void AsyncSerial::IoThreadFunc() {
+    struct epoll_event events[4];
+
+    while (running_) {
+        int n = epoll_wait(epoll_fd_, events, 4, 50);
+
+        if (n <= 0) {
+            WriteToPort(true);
+            continue;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (events[i].data.fd == serial_fd_ &&
+                (events[i].events & EPOLLIN)) {
+                ReadFromPort();
+            }
+        }
+
+        WriteToPort(true);
+    }
+}
 
 void AsyncSerial::ReadFromPort() {
-  auto sthis = shared_from_this();
-  serial_port_.async_read_some(
-      asio::buffer(rx_buf_),
-      [sthis](asio::error_code error, size_t bytes_transferred) {
-        if (error) {
-          sthis->Close();
-          return;
-        }
-
-        if (sthis->rcv_cb_ != nullptr) {
-          sthis->rcv_cb_(sthis->rx_buf_.data(), sthis->rx_buf_.size(),
-                         bytes_transferred);
-        } else {
-          sthis->DefaultReceiveCallback(
-              sthis->rx_buf_.data(), sthis->rx_buf_.size(), bytes_transferred);
-        }
-        sthis->ReadFromPort();
-      });
+    ssize_t len = ::read(serial_fd_, rx_buf_.data(), rx_buf_.size());
+    if (len > 0) {
+        if (rcv_cb_)
+            rcv_cb_(rx_buf_.data(), rx_buf_.size(), len);
+        else
+            DefaultReceiveCallback(rx_buf_.data(), rx_buf_.size(), len);
+    }
 }
 
 void AsyncSerial::WriteToPort(bool check_if_busy) {
-  // do nothing if an async tx has already been initiated
-  if (check_if_busy && tx_in_progress_) return;
+    if (check_if_busy && tx_in_progress_)
+        return;
 
-  std::lock_guard<std::recursive_mutex> lock(tx_mutex_);
-  if (tx_rbuf_.IsEmpty()) return;
+    std::lock_guard<std::recursive_mutex> lock(tx_mutex_);
 
-  auto sthis = shared_from_this();
-  tx_in_progress_ = true;
-  auto len = tx_rbuf_.Read(tx_buf_, tx_rbuf_.GetOccupiedSize());
-  serial_port_.async_write_some(
-      asio::buffer(tx_buf_, len),
-      [sthis](asio::error_code error, size_t bytes_transferred) {
-        if (error) {
-          sthis->Close();
-          return;
-        }
-        std::lock_guard<std::recursive_mutex> lock(sthis->tx_mutex_);
-        if (sthis->tx_rbuf_.IsEmpty()) {
-          sthis->tx_in_progress_ = false;
-          return;
-        } else {
-          sthis->WriteToPort(false);
-        }
-      });
+    if (tx_rbuf_.IsEmpty()) {
+        tx_in_progress_ = false;
+        return;
+    }
+
+    size_t count = tx_rbuf_.Read(tx_buf_, tx_rbuf_.GetOccupiedSize());
+
+    ssize_t written = ::write(serial_fd_, tx_buf_, count);
+    if (written < 0) {
+        tx_in_progress_ = false;
+        return;
+    }
+
+    tx_in_progress_ = (written < (ssize_t)count);
 }
 
 void AsyncSerial::SendBytes(const uint8_t *bytes, size_t length) {
-  if (!IsOpened()) {
-    std::cerr << "Failed to send, port closed" << std::endl;
-    return;
-  }
-  assert(length < rxtx_buffer_size);
-  std::lock_guard<std::recursive_mutex> lock(tx_mutex_);
-  if (tx_rbuf_.GetFreeSize() < length) {
-    throw std::length_error(
-        "AsyncSerial::SendBytes: tx buffer overflow, try to slow down sending "
-        "data");
-  }
-  tx_rbuf_.Write(bytes, length);
-  io_context_.post(
-      std::bind(&AsyncSerial::WriteToPort, shared_from_this(), true));
+    if (!IsOpened()) {
+        std::cerr << "Port closed, cannot send\n";
+        return;
+    }
+
+    if (length >= rxtx_buffer_size) {
+        throw std::length_error("SendBytes: length exceeds buffer size");
+    }
+
+    std::lock_guard<std::recursive_mutex> lock(tx_mutex_);
+
+    if (tx_rbuf_.GetFreeSize() < length) {
+        throw std::length_error("SendBytes: TX buffer overflow");
+    }
+
+    tx_rbuf_.Write(bytes, length);
 }
-}  // namespace westonrobot
+
+} // namespace westonrobot
